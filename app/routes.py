62d4bb31018a7fa50fd7cfd7d79916582ff6fb2a
Kelonk <79749521+Kelonk@users.noreply.github.com>
2021-02-27 17:09:38 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,8 @@ def index1():  # school
 @app.route('/report/<int:id>', methods=['GET'])
 @login_required
 def report(id):  # report
-    sql = db.session.query(Metric, Student).filter(Student.group_id == id).join(Student).statement
+    sql = db.session.query(Metric, Student).join(Student).statement
+    #sql = db.session.query(Metric, Student).filter(Student.group_id == id).join(Student).statement
     # sql = db.session.query(Metric, Student).filter(Metric.student.group.id==id, Student.group_id==id).join(Student).statement
     data = pd.read_sql(sql, db.engine)
     df_1 = pd.DataFrame([], columns=['Имя', 'Первое измерение', 'Второе измерение', 'Третье измерение'])
